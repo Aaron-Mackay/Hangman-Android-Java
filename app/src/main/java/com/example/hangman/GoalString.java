ca97696f5fbaacf23045e66d161fbef3e422ec32
Aaron Mackay <aaronmackay@hotmail.com>
2020-04-27 13:49:48 +0100

    Added a hardcoded goal string (temp), guess checking, and string updating

---
+++ app/src/main/java/com/example/hangman/GoalString.java
@@ -0,0 +1,34 @@
+package com.example.hangman;
+
+class GoalString {
+    private String openString; //actual string
+    private String closedString; // underscore version
+
+    //constructor
+    public GoalString(String openString) {
+        this.openString = openString;
+
+        String temp = "";
+        for (char letter : openString.toCharArray()) {
+            if (Character.isLetter(letter)) {
+                temp = temp + "_";
+            } else {
+                temp = temp + letter;
+            }
+
+        }
+        this.closedString = temp;
+    }
+
+    public String getOpenString() {
+        return openString;
+    }
+
+    public String getClosedString() {
+        return closedString;
+    }
+
+    public void setClosedString(String closedString) {
+        this.closedString = closedString;
+    }
+}
--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,37 +21,69 @@ public class MainActivity extends AppCompatActivity {
     int stage = 1;
     final int guessesAllowed = 11;
     NumberPicker letterPicker;
-    String[] pickerLetters = new String[] {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-    String[] guessedLetters = new String[] {"a", "e"};
+    String[] pickerLetters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    String[] guessedLetters = new String[26];
+    TextView guessedLettersDisplay;
+    TextView goalStringDisplay;
+    GoalString goalString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainImage = findViewById(R.id.mainImage);
+        guessedLettersDisplay = findViewById(R.id.guessedLetters);
+        updateGuessedDisplay(guessedLetters);
+        updatePicker(guessedLetters);
+        goalStringDisplay = findViewById(R.id.goalStringDisplay);
 
-        initilisePicker(guessedLetters);
 
         for (int i = 1; i <= 11; i++) {
             String imageName = String.format("stage%d", i);
             int imageId = getResources().getIdentifier(imageName, "drawable", getApplicationInfo().packageName);
             imageMap.put(imageName, imageId);
         }
+
+
+        //TODO HARDCODED, EDIT
+        goalString = new GoalString("check if the space hasn't");
+        goalStringDisplay.setText(goalString.getClosedString());
+
     }
 
-    public void step(View view) {
-        stage++;
-        if (stage <= guessesAllowed) {
-            mainImage.setImageResource(imageMap.get("stage" + stage));
+    public void onClickStep(View view) {
+        String selectedLetter = pickerLetters[letterPicker.getValue()];
+        boolean guessMatch = checkGuess(selectedLetter, goalString);
+        if (!guessMatch) {
+            stage++;
+            if (stage <= guessesAllowed) {
+                //todo if no guesses left, game over
+                //todo if correct, point
+                //todo add guessed letter to guessedLetters
+
+                for (int i = 0; i < guessedLetters.length; i++) {
+                    if (guessedLetters[i] == null) {
+                        guessedLetters[i] = selectedLetter;
+                        break;
+                    }
+                }
+
+                mainImage.setImageResource(imageMap.get("stage" + stage));
+                updateGuessedDisplay(guessedLetters);
+            }
+
+            updatePicker(guessedLetters);
         }
     }
 
-    public void initilisePicker(String[] guessedLetters) {
+    public void updatePicker(String[] guessedLetters) {
         List<String> temp = new LinkedList<String>(Arrays.asList(pickerLetters));
         int removedLetterCount = 0;
         for (int i = 0; i < pickerLetters.length; i++) {
             for (int j = 0; j < guessedLetters.length; j++) {
-                if (guessedLetters[j].equalsIgnoreCase(pickerLetters[i])) {
+                if (guessedLetters[j] == null) {
+                    break;
+                } else if (guessedLetters[j].equalsIgnoreCase(pickerLetters[i])) {
                     temp.remove(i - removedLetterCount); //removes a letter from the pickerletters as it has been guessed, shifting index backwards to account for removed letters
                     removedLetterCount++;
                     break;
@@ -66,5 +99,38 @@ public class MainActivity extends AppCompatActivity {
         letterPicker.setMinValue(0);
         letterPicker.setMaxValue(pickerLetters.length - 1);
         letterPicker.setDisplayedValues(pickerLetters);
+    }
+
+    public void updateGuessedDisplay(String[] guessedLetters) {
+        String display = "";
+        for (String letter : guessedLetters) {
+            if (letter != null) {
+                display = display + letter + ", ";
+            }
+        }
+        if (display.length() != 0) {
+            display = display.substring(0, display.length() - 2);
+        }
+        guessedLettersDisplay.setText(display);
+    }
+
+    public boolean checkGuess(String guess, GoalString goalString) {
+        char guessChar = guess.charAt(0);
+        boolean match = false;
+        String goalStringOpen = goalString.getOpenString();
+        String goalStringClosed = goalString.getClosedString();
+        String goalStringCurrent = "";
+
+        for (int i = 0; i < goalStringOpen.length(); i++) {
+            if (goalStringOpen.charAt(i) == guessChar) {
+                match = true;
+                goalStringCurrent = goalStringCurrent + guessChar;
+            } else {
+                goalStringCurrent = goalStringCurrent + goalStringClosed.charAt(i);
+            }
+        }
+        goalString.setClosedString(goalStringCurrent); //updates with revealed string
+        goalStringDisplay.setText(goalStringCurrent);
+        return match;
     }
 }
